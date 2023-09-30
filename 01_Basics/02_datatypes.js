4419e2b6755b82468dac1b5048675eaf2e75a7d3
pradvik <88715560+pradvik@users.noreply.github.com>
2023-09-30 22:24:24 +0000

    changes done

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,18 @@
+// number => 2 to power 53
+// bigint => number bigger than 2 to power 53
+// String => "everything written inside the double quotes :)"
+// boolean => true/false
+// null => standalone value // null is also a object (not clear )
+// undefined => when value is not defined to the variable.
+// symbol => to find uniqueness in components (assumption)
+// object =>
+
+
+
+//code
+
+let car = null
+console.log(typeof car) // output will the (object)
+
+let bike;
+console.log(typeof bike)// bike has been not defined a value no (undefined)
